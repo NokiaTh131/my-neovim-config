74026ba72867df30da2fd37a3489313dc8c3cfa8
NokiaTh131 <nokiath131@gmail.com>
2025-07-10 22:55:07 +0700

    removed unecessary config from blink.cmp

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -12,29 +12,7 @@ return {
         use_nvim_cmp_as_default = true,
         nerd_font_variant = 'mono'
       },
-      
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' }
-      },
-      
-      completion = {
-        accept = {
-          auto_brackets = {
-            enabled = true
-          }
-        },
-        menu = {
-          border = 'none'
-        },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 500
-        },
-        ghost_text = {
-          enabled = true
-        }
-      },
-      
+     
       signature = {
         enabled = true
       }
