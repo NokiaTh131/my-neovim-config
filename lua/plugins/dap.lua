56b3fc35ffe880282a020675e6bcc0aa81d9450e
NokiaTh131 <nokiath131@gmail.com>
2025-07-19 14:09:09 +0700

    config nvim dap

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -79,7 +79,7 @@ return {
           type = "codelldb",
           request = "launch",
           program = function()
-            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
           end,
           cwd = "${workspaceFolder}",
           stopOnEntry = false,
@@ -87,10 +87,10 @@ return {
       }
 
       -- Key mappings
-      vim.keymap.set("n", "<F5>", dap.continue, { desc = "Debug: Start/Continue" })
-      vim.keymap.set("n", "<F1>", dap.step_into, { desc = "Debug: Step Into" })
-      vim.keymap.set("n", "<F2>", dap.step_over, { desc = "Debug: Step Over" })
-      vim.keymap.set("n", "<F3>", dap.step_out, { desc = "Debug: Step Out" })
+      vim.keymap.set("n", "<F1>", dap.continue, { desc = "Debug: Start/Continue" })
+      vim.keymap.set("n", "<F2>", dap.step_into, { desc = "Debug: Step Into" })
+      vim.keymap.set("n", "<F3>", dap.step_over, { desc = "Debug: Step Over" })
+      vim.keymap.set("n", "<F4>", dap.step_out, { desc = "Debug: Step Out" })
       vim.keymap.set("n", "<leader>b", dap.toggle_breakpoint, { desc = "Debug: Toggle Breakpoint" })
       vim.keymap.set("n", "<leader>B", function()
         dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
@@ -109,4 +109,3 @@ return {
     opts = {},
   },
 }
-
