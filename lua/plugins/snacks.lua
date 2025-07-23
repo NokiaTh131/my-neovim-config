4e63abe615b98bb4f61162ffc67d3fafa7993e0f
NokiaTh131 <nokiath131@gmail.com>
2025-07-24 01:48:28 +0700

    add background color to float window

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,196 +1,418 @@
 return {
-  {
-    "folke/snacks.nvim",
-    priority = 1000,
-    lazy = false,
-    opts = {
-      -- Dashboard
-      dashboard = {
-        enabled = true,
-        sections = {
-          { section = "header" },
-          { section = "keys",   gap = 1, padding = 1 },
-          { section = "startup" },
-        },
-      },
+	{
+		"folke/snacks.nvim",
+		priority = 1000,
+		lazy = false,
+		opts = {
+			-- Dashboard
+			dashboard = {
+				enabled = true,
+				sections = {
+					{ section = "header" },
+					{ section = "keys", gap = 1, padding = 1 },
+					{ section = "startup" },
+				},
+			},
+			-- Notifications
+			notifier = {
+				enabled = true,
+				timeout = 3000,
+				width = { min = 40, max = 0.4 },
+				height = { min = 1, max = 0.6 },
+				margin = { top = 0, right = 1, bottom = 0 },
+				padding = true,
+				sort = { "level", "added" },
+				level = vim.log.levels.TRACE,
+				icons = {
+					error = "",
+					warn = "",
+					info = "",
+					debug = "",
+					trace = "",
+				},
+				keep = function()
+					return vim.fn.getcmdtype() ~= ""
+				end,
+			},
 
-      -- Notifications
-      notifier = {
-        enabled = true,
-        timeout = 3000,
-        width = { min = 40, max = 0.4 },
-        height = { min = 1, max = 0.6 },
-        margin = { top = 0, right = 1, bottom = 0 },
-        padding = true,
-        sort = { "level", "added" },
-        level = vim.log.levels.TRACE,
-        icons = {
-          error = "",
-          warn = "",
-          info = "",
-          debug = "",
-          trace = "",
-        },
-        keep = function()
-          return vim.fn.getcmdtype() ~= ""
-        end,
-      },
+			-- Terminal
+			terminal = {
+				enabled = true,
+			},
 
-      -- Terminal
-      terminal = {
-        enabled = true,
-      },
+			-- Zen mode
+			zen = {
+				enabled = true,
+				toggles = {
+					dim = true,
+					git_signs = false,
+					mini_diff_signs = false,
+					diagnostics = false,
+					inlay_hints = false,
+				},
+				show = {
+					statusline = false,
+					tabline = false,
+				},
+				win = {
+					backdrop = {
+						transparent = false,
+						blend = 95,
+					},
+				},
+				zoom = {
+					toggles = {},
+					show = {
+						statusline = true,
+						tabline = true,
+					},
+					win = {
+						backdrop = {
+							transparent = false,
+							blend = 95,
+						},
+						width = 120,
+					},
+				},
+			},
 
-      -- Zen mode
-      zen = {
-        enabled = true,
-        toggles = {
-          dim = true,
-          git_signs = false,
-          mini_diff_signs = false,
-          diagnostics = false,
-          inlay_hints = false,
-        },
-        show = {
-          statusline = false,
-          tabline = false,
-        },
-        win = {
-          backdrop = {
-            transparent = false,
-            blend = 95,
-          }
-        },
-        zoom = {
-          toggles = {},
-          show = {
-            statusline = true,
-            tabline = true,
-          },
-          win = {
-            backdrop = {
-              transparent = false,
-              blend = 95,
-            },
-            width = 120,
-          },
-        },
-      },
+			-- Scope
+			scope = { enabled = true },
 
-      -- Scope
-      scope = { enabled = true },
+			-- Quick file
+			quickfile = { enabled = true },
 
-      -- Quick file
-      quickfile = { enabled = true },
+			-- Status column
+			statuscolumn = { enabled = true },
 
-      -- Status column
-      statuscolumn = { enabled = true },
+			-- Words
+			words = { enabled = true },
 
-      -- Words
-      words = { enabled = true },
+			-- Picker
+			picker = { enabled = true },
 
-      -- Picker
-      picker = { enabled = true },
+			-- Rename
+			rename = { enabled = true },
 
-      -- Rename
-      rename = { enabled = true },
+			-- Styles
+			styles = {
+				notification = {
+					wo = { wrap = true },
+				},
+			},
+		},
+		keys = {
+			{
+				"<leader>z",
+				function()
+					Snacks.zen()
+				end,
+				desc = "Toggle Zen Mode",
+			},
+			{
+				"<leader>Z",
+				function()
+					Snacks.zen.zoom()
+				end,
+				desc = "Toggle Zoom",
+			},
+			{
+				"<leader>.",
+				function()
+					Snacks.scratch()
+				end,
+				desc = "Toggle Scratch Buffer",
+			},
+			{
+				"<leader>S",
+				function()
+					Snacks.scratch.select()
+				end,
+				desc = "Select Scratch Buffer",
+			},
+			{
+				"<leader>n",
+				function()
+					Snacks.notifier.show_history()
+				end,
+				desc = "Notification History",
+			},
+			{
+				"<leader>bd",
+				function()
+					Snacks.bufdelete()
+				end,
+				desc = "Delete Buffer",
+			},
+			{
+				"<leader>cR",
+				function()
+					Snacks.rename.rename_file()
+				end,
+				desc = "Rename File",
+			},
+			{
+				"<leader>un",
+				function()
+					Snacks.notifier.hide()
+				end,
+				desc = "Dismiss All Notifications",
+			},
+			-- File operations
+			{
+				"<leader>ff",
+				function()
+					Snacks.picker.files()
+				end,
+				desc = "Find Files",
+			},
+			{
+				"<leader>fg",
+				function()
+					Snacks.picker.grep()
+				end,
+				desc = "Live Grep",
+			},
+			{
+				"<leader>fb",
+				function()
+					Snacks.picker.buffers()
+				end,
+				desc = "Find Buffers",
+			},
+			{
+				"<leader>fh",
+				function()
+					Snacks.picker.help()
+				end,
+				desc = "Help Tags",
+			},
+			{
+				"<leader>fr",
+				function()
+					Snacks.picker.recent()
+				end,
+				desc = "Recent Files",
+			},
+			{
+				"<leader>fw",
+				function()
+					Snacks.picker.grep_word()
+				end,
+				desc = "Grep Word Under Cursor",
+			},
 
-      -- Styles
-      styles = {
-        notification = {
-          wo = { wrap = true },
-        },
-      },
-    },
-    keys = {
-      { "<leader>z",  function() Snacks.zen() end,                           desc = "Toggle Zen Mode" },
-      { "<leader>Z",  function() Snacks.zen.zoom() end,                      desc = "Toggle Zoom" },
-      { "<leader>.",  function() Snacks.scratch() end,                       desc = "Toggle Scratch Buffer" },
-      { "<leader>S",  function() Snacks.scratch.select() end,                desc = "Select Scratch Buffer" },
-      { "<leader>n",  function() Snacks.notifier.show_history() end,         desc = "Notification History" },
-      { "<leader>bd", function() Snacks.bufdelete() end,                     desc = "Delete Buffer" },
-      { "<leader>cR", function() Snacks.rename.rename_file() end,            desc = "Rename File" },
-      { "<leader>un", function() Snacks.notifier.hide() end,                 desc = "Dismiss All Notifications" },
-      -- File operations
-      { "<leader>ff", function() Snacks.picker.files() end,                  desc = "Find Files" },
-      { "<leader>fg", function() Snacks.picker.grep() end,                   desc = "Live Grep" },
-      { "<leader>fb", function() Snacks.picker.buffers() end,                desc = "Find Buffers" },
-      { "<leader>fh", function() Snacks.picker.help() end,                   desc = "Help Tags" },
-      { "<leader>fr", function() Snacks.picker.recent() end,                 desc = "Recent Files" },
-      { "<leader>fw", function() Snacks.picker.grep_word() end,              desc = "Grep Word Under Cursor" },
+			-- LSP & Diagnostics
+			{
+				"<leader>lr",
+				function()
+					Snacks.picker.lsp_references()
+				end,
+				desc = "LSP References",
+			},
+			{
+				"<leader>ld",
+				function()
+					Snacks.picker.lsp_definitions()
+				end,
+				desc = "LSP Definitions",
+			},
+			{
+				"<leader>li",
+				function()
+					Snacks.picker.lsp_implementations()
+				end,
+				desc = "LSP Implementations",
+			},
+			{
+				"<leader>ls",
+				function()
+					Snacks.picker.lsp_symbols()
+				end,
+				desc = "LSP Document Symbols",
+			},
+			{
+				"<leader>lS",
+				function()
+					Snacks.picker.lsp_workspace_symbols()
+				end,
+				desc = "LSP Workspace Symbols",
+			},
+			{
+				"<leader>xx",
+				function()
+					Snacks.picker.diagnostics()
+				end,
+				desc = "Diagnostics",
+			},
+			{
+				"<leader>xb",
+				function()
+					Snacks.picker.diagnostics_buffer()
+				end,
+				desc = "Buffer Diagnostics",
+			},
 
-      -- LSP & Diagnostics
-      { "<leader>lr", function() Snacks.picker.lsp_references() end,         desc = "LSP References" },
-      { "<leader>ld", function() Snacks.picker.lsp_definitions() end,        desc = "LSP Definitions" },
-      { "<leader>li", function() Snacks.picker.lsp_implementations() end,    desc = "LSP Implementations" },
-      { "<leader>ls", function() Snacks.picker.lsp_symbols() end,            desc = "LSP Document Symbols" },
-      { "<leader>lS", function() Snacks.picker.lsp_workspace_symbols() end,  desc = "LSP Workspace Symbols" },
-      { "<leader>xx", function() Snacks.picker.diagnostics() end,            desc = "Diagnostics" },
-      { "<leader>xb", function() Snacks.picker.diagnostics_buffer() end,     desc = "Buffer Diagnostics" },
+			-- Navigation & Search
+			{
+				"<leader>fk",
+				function()
+					Snacks.picker.keymaps()
+				end,
+				desc = "Find Keymaps",
+			},
+			{
+				"<leader>fc",
+				function()
+					Snacks.picker.commands()
+				end,
+				desc = "Find Commands",
+			},
+			{
+				"<leader>fo",
+				function()
+					Snacks.picker.oldfiles()
+				end,
+				desc = "Old Files",
+			},
+			{
+				"<leader>fj",
+				function()
+					Snacks.picker.jumps()
+				end,
+				desc = "Jump List",
+			},
+			{
+				"<leader>fm",
+				function()
+					Snacks.picker.marks()
+				end,
+				desc = "Marks",
+			},
+			{
+				"<leader>fq",
+				function()
+					Snacks.picker.qflist()
+				end,
+				desc = "Quickfix List",
+			},
+			{
+				"<leader>fl",
+				function()
+					Snacks.picker.loclist()
+				end,
+				desc = "Location List",
+			},
 
-      -- Navigation & Search
-      { "<leader>fk", function() Snacks.picker.keymaps() end,                desc = "Find Keymaps" },
-      { "<leader>fc", function() Snacks.picker.commands() end,               desc = "Find Commands" },
-      { "<leader>fo", function() Snacks.picker.oldfiles() end,               desc = "Old Files" },
-      { "<leader>fj", function() Snacks.picker.jumps() end,                  desc = "Jump List" },
-      { "<leader>fm", function() Snacks.picker.marks() end,                  desc = "Marks" },
-      { "<leader>fq", function() Snacks.picker.qflist() end,                 desc = "Quickfix List" },
-      { "<leader>fl", function() Snacks.picker.loclist() end,                desc = "Location List" },
+			-- Advanced
+			{
+				"<leader>f.",
+				function()
+					Snacks.picker.files({ hidden = true })
+				end,
+				desc = "Find All Files (including hidden)",
+			},
+			{
+				"<leader>f/",
+				function()
+					Snacks.picker.lines()
+				end,
+				desc = "Search Lines in Open Buffers",
+			},
+			{
+				"<leader>f:",
+				function()
+					Snacks.picker.command_history()
+				end,
+				desc = "Command History",
+			},
+			{
+				"<leader>f;",
+				function()
+					Snacks.picker.search_history()
+				end,
+				desc = "Search History",
+			},
+			{
+				"<c-/>",
+				function()
+					Snacks.terminal()
+				end,
+				desc = "Toggle Terminal",
+				mode = { "n", "t" },
+			},
+			{
+				"<c-_>",
+				function()
+					Snacks.terminal()
+				end,
+				desc = "Toggle Terminal",
+				mode = { "n", "t" },
+			},
+			{
+				"]]",
+				function()
+					Snacks.words.jump(vim.v.count1)
+				end,
+				desc = "Next Reference",
+				mode = { "n", "t" },
+			},
+			{
+				"[[",
+				function()
+					Snacks.words.jump(-vim.v.count1)
+				end,
+				desc = "Prev Reference",
+				mode = { "n", "t" },
+			},
+			{
+				"<leader>N",
+				desc = "Neovim News",
+				function()
+					Snacks.win({
+						file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
+						width = 0.6,
+						height = 0.6,
+						wo = {
+							spell = false,
+							wrap = false,
+							signcolumn = "yes",
+							statuscolumn = " ",
+							conceallevel = 3,
+						},
+					})
+				end,
+			},
+		},
+		init = function()
+			vim.api.nvim_create_autocmd("User", {
+				pattern = "VeryLazy",
+				callback = function()
+					-- Setup some globals for debugging (lazy-loaded)
+					_G.dd = function(...)
+						Snacks.debug.inspect(...)
+					end
+					_G.bt = function()
+						Snacks.debug.backtrace()
+					end
+					vim.print = _G.dd -- Override print to use snacks for `:=` command
 
-      -- Advanced
-      { "<leader>f.", function() Snacks.picker.files({ hidden = true }) end, desc = "Find All Files (including hidden)" },
-      { "<leader>f/", function() Snacks.picker.lines() end,                  desc = "Search Lines in Open Buffers" },
-      { "<leader>f:", function() Snacks.picker.command_history() end,        desc = "Command History" },
-      { "<leader>f;", function() Snacks.picker.search_history() end,         desc = "Search History" },
-      { "<c-/>",      function() Snacks.terminal() end,                      desc = "Toggle Terminal",                  mode = { "n", "t" } },
-      { "<c-_>",      function() Snacks.terminal() end,                      desc = "Toggle Terminal",                  mode = { "n", "t" } },
-      { "]]",         function() Snacks.words.jump(vim.v.count1) end,        desc = "Next Reference",                   mode = { "n", "t" } },
-      { "[[",         function() Snacks.words.jump(-vim.v.count1) end,       desc = "Prev Reference",                   mode = { "n", "t" } },
-      {
-        "<leader>N",
-        desc = "Neovim News",
-        function()
-          Snacks.win({
-            file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-            width = 0.6,
-            height = 0.6,
-            wo = {
-              spell = false,
-              wrap = false,
-              signcolumn = "yes",
-              statuscolumn = " ",
-              conceallevel = 3,
-            },
-          })
-        end,
-      },
-    },
-    init = function()
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "VeryLazy",
-        callback = function()
-          -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
-          vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-          -- Create some toggle mappings
-          Snacks.toggle.option("spell", { name = "spelling" }):map("<leader>us")
-          Snacks.toggle.option("wrap", { name = "wrap" }):map("<leader>uw")
-          Snacks.toggle.option("relativenumber", { name = "relative number" }):map("<leader>uL")
-          Snacks.toggle.diagnostics():map("<leader>ud")
-          Snacks.toggle.line_number():map("<leader>ul")
-          Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
-            "<leader>uc")
-          Snacks.toggle.treesitter():map("<leader>uT")
-          Snacks.toggle.option("background", { off = "light", on = "dark", name = "dark background" }):map("<leader>ub")
-          Snacks.toggle.inlay_hints():map("<leader>uh")
-        end,
-      })
-    end,
-  },
+					-- Create some toggle mappings
+					Snacks.toggle.option("spell", { name = "spelling" }):map("<leader>us")
+					Snacks.toggle.option("wrap", { name = "wrap" }):map("<leader>uw")
+					Snacks.toggle.option("relativenumber", { name = "relative number" }):map("<leader>uL")
+					Snacks.toggle.diagnostics():map("<leader>ud")
+					Snacks.toggle.line_number():map("<leader>ul")
+					Snacks.toggle
+						.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
+						:map("<leader>uc")
+					Snacks.toggle.treesitter():map("<leader>uT")
+					Snacks.toggle
+						.option("background", { off = "light", on = "dark", name = "dark background" })
+						:map("<leader>ub")
+					Snacks.toggle.inlay_hints():map("<leader>uh")
+				end,
+			})
+		end,
+	},
 }
