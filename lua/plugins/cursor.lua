e6be1c97b180b33893f7d8445ed7de02f5a0eb21
NokiaTh131 <nokiath131@gmail.com>
2025-07-24 15:50:02 +0700

    better animation

--- lua/plugins/cursor.lua
+++ lua/plugins/cursor.lua
@@ -4,8 +4,8 @@ return {
 		enabled = true,
 		cursor_color = "#e0def4",
 		smear_between_buffers = false,
-		stiffness = 0.9,
-		trailing_stiffness = 0.8,
-		distance_stop_animating = 0.3,
+		stiffness = 0.8,
+		trailing_stiffness = 0.5,
+		distance_stop_animating = 0.5,
 	},
 }
