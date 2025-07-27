b8cdd47f7905537092869a31f7bc4831316d6345
NokiaTh131 <nokiath131@gmail.com>
2025-07-27 15:20:15 +0700

    fix bug that lualine not showup

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -23,6 +23,9 @@ return {
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup()
+		end,
 	},
 	{
 		"akinsho/bufferline.nvim",
