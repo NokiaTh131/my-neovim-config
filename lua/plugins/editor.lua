12eec94f18507a67921808fb117f68c8984f99a4
NokiaTh131 <nokiath131@gmail.com>
2025-07-27 16:47:14 +0700

    better lualine information

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -24,7 +24,43 @@ return {
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
-			require("lualine").setup()
+			require("lualine").setup({
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = { "branch", "diff", "diagnostics", "filename" },
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {
+						"filetype",
+						{
+							"lsp_status",
+							icon = "", -- f013
+							symbols = {
+								-- Standard unicode symbols to cycle through for LSP progress:
+								spinner = { "⠋", "⠙", "⠹", "⠸", "⠼", "⠴", "⠦", "⠧", "⠇", "⠏" },
+								-- Standard unicode symbol for when LSP is done:
+								done = "✓",
+								-- Delimiter inserted between LSP names:
+								separator = " ",
+							},
+							-- List of LSP names to ignore (e.g., `null-ls`):
+							ignore_lsp = {},
+						},
+						"progress",
+					},
+					lualine_z = {
+						"location",
+					},
+				},
+				inactive_sections = {
+					lualine_a = { "filename" },
+					lualine_b = {},
+					lualine_c = {},
+					lualine_x = {},
+					lualine_y = {},
+					lualine_z = { "location" },
+				},
+			})
 		end,
 	},
 	{
