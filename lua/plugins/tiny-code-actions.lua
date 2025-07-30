a2341efecb871d92778e23df006a6a1cecc35b81
NokiaTh131 <nokiath131@gmail.com>
2025-07-31 02:55:07 +0700

    config code action a little bit

--- lua/plugins/tiny-code-actions.lua
+++ lua/plugins/tiny-code-actions.lua
@@ -14,6 +14,26 @@ return {
 		opts = {
 			backend = "delta",
 			picker = "snacks",
+			backend_opts = {
+				delta = {
+					header_lines_to_remove = 4,
+					args = {
+						"--line-numbers",
+					},
+				},
+			},
+			signs = {
+				quickfix = { "", { link = "DiagnosticWarning" } },
+				others = { "", { link = "DiagnosticWarning" } },
+				refactor = { "", { link = "DiagnosticInfo" } },
+				["refactor.move"] = { "󰪹", { link = "DiagnosticInfo" } },
+				["refactor.extract"] = { "", { link = "DiagnosticError" } },
+				["source.organizeImports"] = { "", { link = "DiagnosticWarning" } },
+				["source.fixAll"] = { "󰃢", { link = "DiagnosticError" } },
+				["source"] = { "", { link = "DiagnosticError" } },
+				["rename"] = { "󰑕", { link = "DiagnosticWarning" } },
+				["codeAction"] = { "", { link = "DiagnosticWarning" } },
+			},
 		},
 	},
 }
