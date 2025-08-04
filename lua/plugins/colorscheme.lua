7d90409c5cd407f4deaf5f95b1a001b4d8cea8a4
NokiaTh131 <nokiath131@gmail.com>
2025-08-04 19:00:34 +0700

    remove gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -76,15 +76,6 @@ return {
 		},
 	},
 	{
-		"sainnhe/gruvbox-material",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.g.gruvbox_material_transparent_background = true
-			vim.g.gruvbox_material_background = "hard"
-		end,
-	},
-	{
 		"projekt0n/github-nvim-theme",
 		name = "github-theme",
 		lazy = false,
