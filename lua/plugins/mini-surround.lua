85b78a8331fd161d264cd4eae99933b34f7c7364
NokiaTh131 <nokiath131@gmail.com>
2025-08-08 02:16:24 +0700

    change to use mini surround

---
+++ lua/plugins/mini-surround.lua
@@ -0,0 +1,17 @@
+return {
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		opts = {
+			mappings = {
+				add = "aa", -- Add surrounding in Normal and Visual modes
+				delete = "ad", -- Delete surrounding
+				find = "af", -- Find surrounding (to the right)
+				find_left = "aF", -- Find surrounding (to the left)
+				highlight = "ah", -- Highlight surrounding
+				replace = "ar", -- Replace surrounding
+				update_n_lines = "an", -- Update `n_lines`
+			},
+		},
+	},
+}
