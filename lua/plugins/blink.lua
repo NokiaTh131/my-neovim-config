1f9315fcf7612fd4dace30119ceb59e419ec1e30
NokiaTh131 <nokiath131@gmail.com>
2025-08-13 02:19:59 +0700

    feature: mini icon in blink

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -37,7 +37,30 @@ return {
 			},
 
 			completion = {
-				menu = { border = "rounded" },
+				ghost_text = { enabled = false },
+				menu = {
+					border = "rounded",
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+									return kind_icon
+								end,
+								highlight = function(ctx)
+									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+									return hl
+								end,
+							},
+							kind = {
+								highlight = function(ctx)
+									local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+									return hl
+								end,
+							},
+						},
+					},
+				},
 				documentation = {
 					auto_show = true,
 					auto_show_delay_ms = 500,
