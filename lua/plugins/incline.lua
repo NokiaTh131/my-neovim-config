8018d928121343d203b6d0ac234c4bd090be4307
NokiaTh131 <nokiath131@gmail.com>
2025-08-15 01:52:50 +0700

    fix: incline icon

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -4,20 +4,20 @@ return {
 		config = function()
 			require("incline").setup({
 				render = function(props)
+					local mini_icons = require("mini.icons")
 					local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ":t")
-					local ft_icon, ft_color = require("mini.icons").get("filetype", vim.bo[props.buf].filetype)
+					if filename == "" then
+						filename = "[No Name]"
+					end
+					local ft_icon, ft_color = mini_icons.get("file", filename)
 					local modified = vim.bo[props.buf].modified
-
 					return {
-						ft_icon and { " ", ft_icon, " ", guifg = ft_color } or "",
+						" ",
+						ft_icon and { ft_icon, " ", guibg = "none", group = ft_color } or "",
 						" ",
 						{ filename, gui = modified and "bold,italic" or "bold" },
 					}
 				end,
-				window = {
-					padding = 1,
-					margin = { horizontal = 1 },
-				},
 			})
 		end,
 		event = "VeryLazy",
