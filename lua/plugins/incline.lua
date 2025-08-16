6504c5fd829e1dc4db8e3c87abd262d5d73604d8
NokiaTh131 <nokiath131@gmail.com>
2025-08-16 22:41:39 +0700

    add diagnostic and git diff signs to incline

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -12,7 +12,41 @@ return {
 					local ft_icon, ft_color = mini_icons.get("file", filename)
 					local has_errors = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity.ERROR }) > 0
 					local modified = vim.bo[props.buf].modified
+					local function get_git_diff()
+						local icons = { removed = "", changed = "", added = "" }
+						icons["changed"] = icons.modified
+						local signs = vim.b[props.buf].gitsigns_status_dict
+						local labels = {}
+						if signs == nil then
+							return labels
+						end
+						for name, icon in pairs(icons) do
+							if tonumber(signs[name]) and signs[name] > 0 then
+								table.insert(labels, { icon .. " ", signs[name] .. " ", group = "Diff" .. name })
+							end
+						end
+						return labels
+					end
+					local function get_diagnostic_label()
+						local icons = { error = "󰅚", warn = "󰀪", info = "󰌶", hint = "󰋽" }
+						local label = {}
+
+						for severity, icon in pairs(icons) do
+							local n = #vim.diagnostic.get(
+								props.buf,
+								{ severity = vim.diagnostic.severity[string.upper(severity)] }
+							)
+							if n > 0 then
+								table.insert(label, { icon .. " ", n .. " ", group = "DiagnosticSign" .. severity })
+							end
+						end
+						return label
+					end
 					return {
+						" ",
+						get_git_diff(),
+						" ",
+						get_diagnostic_label(),
 						" ",
 						ft_icon and { ft_icon, " ", group = ft_color } or "",
 						" ",
